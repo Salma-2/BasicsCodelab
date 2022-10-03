5923947a2b716e5e9a8a62df8964f2fb071cc9e4
= <=>
2022-10-03 15:04:21 +0200

    created onboarding screen , and greeting screen with 2 greetings only

--- app/src/main/java/com/salma/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/salma/basicscodelab/MainActivity.kt
@@ -3,14 +3,18 @@ package com.salma.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.salma.basicscodelab.ui.theme.BasicsCodelabTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,23 +22,82 @@ class MainActivity : ComponentActivity() {
         setContent {
             BasicsCodelabTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                MyApp()
+            }
+        }
+    }
+
+    @Composable
+    fun GreetingsScreen(names: List<String> = listOf("World", "Compose")) {
+        Surface(
+            color = MaterialTheme.colors.background) {
+            Column(modifier = Modifier.padding(vertical = 4.dp)) {
+                for (name in names) {
+                    Greeting(name)
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    fun Greeting(name: String) {
+        val expanded = remember {
+            mutableStateOf(false)
+        }
+        val extraPadding = if (expanded.value) 48.dp else 0.dp
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BasicsCodelabTheme {
-        Greeting("Android")
+        Surface(color = MaterialTheme.colors.primary,
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+            Row(modifier = Modifier
+                .padding(24.dp)) {
+                Column(Modifier
+                    .weight(1f)
+                    .padding(vertical = extraPadding)) {
+                    Text(text = "Hello ")
+                    Text(text = "$name!")
+                }
+                OutlinedButton(
+                    onClick = { expanded.value = !expanded.value }) {
+                    Text(if (expanded.value) "Show less" else "Show more")
+                }
+            }
+        }
     }
+
+    @Composable
+    fun OnBoardingScreen(onContinueClicked: () -> Unit) {
+        Column(modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(24.dp),
+                onClick = onContinueClicked) {
+                Text(text = "Continue")
+            }
+        }
+    }
+
+    @Preview(showBackground = true, widthDp = 320)
+    @Composable
+    fun DefaultPreview() {
+        BasicsCodelabTheme {
+            OnBoardingScreen(onContinueClicked = {})
+        }
+    }
+
+    @Composable
+    private fun MyApp() {
+        var shouldShowOnBoarding by remember {
+            mutableStateOf(true)
+        }
+        if (shouldShowOnBoarding) {
+            OnBoardingScreen(onContinueClicked = { shouldShowOnBoarding = false })
+        } else {
+            GreetingsScreen()
+        }
+    }
+
+
 }

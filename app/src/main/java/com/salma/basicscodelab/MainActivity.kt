8ecbc8e1e61e0ceb86235a5e5eb2a30984aca456
= <=>
2022-10-03 15:31:36 +0200

    styled and themed the app

--- app/src/main/java/com/salma/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/salma/basicscodelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.salma.basicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -21,6 +22,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,7 +69,9 @@ class MainActivity : ComponentActivity() {
                     .weight(1f)
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                     Text(text = "Hello ")
-                    Text(text = "$name!")
+                    Text(text = name, style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    ))
                 }
                 OutlinedButton(
                     onClick = { expanded.value = !expanded.value }) {
@@ -94,6 +98,12 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+
+    @Preview(showBackground = true,
+        widthDp = 320,
+        uiMode = UI_MODE_NIGHT_YES,
+        name = "DefaultPreviewDark"
+    )
     @Preview(showBackground = true, widthDp = 320)
     @Composable
     fun DefaultPreview() {

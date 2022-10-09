059e96049dd9bd6a6e17ac48517b237f91437bd2
= <=>
2022-10-09 11:34:38 +0200

    Add colors for the theme

---
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/Theme.kt
@@ -0,0 +1,11 @@
+package com.codelab.theming.ui
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun JetnewsTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colors= LightColors,
+        content = content)
+}
